91558c5b482e7ed3a6bba6f93b4c25d7b63f0243
Muhammad Reza Fahlevi <rezzvy@hotmail.com>
2023-12-31 04:33:41 +0800

    Split MVC component into separate files.

---
+++ src/mvc/controller.js
@@ -0,0 +1,53 @@
+export default class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  init() {
+    for (let i = 0; i < 4; i++) {
+      this.view.generateBoard();
+    }
+
+    this.view.addBoardButton.addEventListener("click", () => {
+      this.view.generateBoard();
+    });
+
+    this.view.uploadImageInput.addEventListener("change", (e) => {
+      this.uploadImageHandler(e);
+    });
+
+    this.view.exportButton.addEventListener("click", (e) => {
+      this.exportHandler();
+    });
+
+    this.view.importFileInput.addEventListener("input", (e) => {
+      this.importHandler(e);
+    });
+  }
+
+  uploadImageHandler(e) {
+    this.view.generateImageListCard(URL.createObjectURL(e.target.files[0]));
+  }
+
+  importHandler(e) {
+    this.model.importData(e.currentTarget.files[0], (e) => {
+      this.view.import(e);
+    });
+  }
+
+  exportHandler() {
+    const convertImageCompleteCallback = () => {
+      this.model.setProjectData({
+        title: this.view.projectTitleElement.textContent,
+        description: this.view.projectDescriptionElement.textContent,
+        board: this.view.getBoardContent(),
+        imgList: this.view.getImageListContent(),
+      });
+
+      this.view.generateGhostAnchor(this.model.exportData(), this.model.projectData.title);
+    };
+
+    this.view.convertImages(convertImageCompleteCallback);
+  }
+}
---
+++ src/mvc/model.js
@@ -0,0 +1,35 @@
+export default class Model {
+  constructor() {
+    this.projectData = {
+      title: "",
+      description: "",
+      board: "",
+      imgList: "",
+    };
+  }
+
+  setProjectData(obj) {
+    const { title, description, board, imgList } = obj;
+
+    this.projectData.title = title;
+    this.projectData.description = description;
+    this.projectData.board = board;
+    this.projectData.imgList = imgList;
+  }
+
+  importData(json, callback) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      this.projectData = JSON.parse(e.target.result);
+      callback(this.projectData);
+    };
+
+    reader.readAsText(json);
+  }
+
+  exportData() {
+    const blob = new Blob([JSON.stringify(this.projectData)], { type: "application/json" });
+    return URL.createObjectURL(blob);
+  }
+}
