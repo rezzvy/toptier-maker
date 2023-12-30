f1055f2b92bbdf3b56235ae89734d73018187cf8
Muhammad Reza Fahlevi <rezzvy@hotmail.com>
2023-12-30 23:30:03 +0800

    Implement code for handling input files.

--- src/app.js
+++ src/app.js
@@ -5,11 +5,23 @@ class View {
     this.addBoardButton = document.getElementById("add-board-btn");
     this.boardContainer = document.querySelector(".tier-list-box-container");
 
+    this.uploadImageContainer = document.querySelector(".img-list-drag");
+    this.uploadImageInput = document.getElementById("input-image");
+
     this.colors = ["#ff7777", "#ffee77", "#77ff7d", "#77a3ff", "#a777ff", "#f277ff"];
   }
 
   getRandomColor() {
     return this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+
+  generateImageListCard(imgSrc) {
+    let placeholder = `<img src="${imgSrc}" />`;
+    const div = document.createElement("div");
+    div.classList.add("img-list-drag-item");
+    div.innerHTML = placeholder;
+
+    this.uploadImageContainer.append(div);
   }
 
   generateBoard() {
@@ -39,6 +51,12 @@ class Controller {
   init() {
     this.view.addBoardButton.addEventListener("click", (e) => {
       this.view.generateBoard();
+    });
+
+    this.view.uploadImageInput.addEventListener("change", (e) => {
+      const blob = URL.createObjectURL(e.target.files[0]);
+
+      this.view.generateImageListCard(blob)
     });
   }
 }
